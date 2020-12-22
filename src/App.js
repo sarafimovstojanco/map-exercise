19b5ae7f92d34f7ee8ee17da7243abe4ab246897
sarafimovstojanco <sarafimovstojanco@gmail.com>
2020-12-22 23:28:27 +0100

    problem so map madafaka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+  const [books, setBooks] = useState('')
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+  const fetchData= () => {
+    return axios.get(apiURL).then(response => setBooks(response.data))
+  }
+  
+ books &&
+    books.map((book, index) => {
+      const cleanedDate = new Date(book.released).toDateString();
+      const authors = book.authors.join(', ');
+      const name = book.name
+      const country = book.country
+      const numberOfPages = book.numberOfPages
+      console.log(['authors'], authors)
+      console.log(['cleanedDate'], cleanedDate)
+      console.log(['name'], name)
+      console.log(['country'], country)
+      console.log(['numberOfPages'], numberOfPages)
+      
+    })
+//  const book = books.map((book, index) => {
+//      date = book.released
+//      authors = book.authors
+//      name = book.name
+//      country = book.country
+//      numberOfPages = book.numberOfPages
+//   })
 
+  return (
+    <div className="App" >
+        <button className="fetch-button" onClick={fetchData}>Fetch Data</button>
+        <div>
+          {books.cleanedDate}
+        </div>
+    </div>
+)}
 export default App;
