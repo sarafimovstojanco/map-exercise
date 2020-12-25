d07fa60ea34f30f5592b5f0ac5168e55696d2405
boka18 <bokasmoka21@gmail.com>
2020-12-25 22:46:06 +0100

    increments/decrements counters

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
 import React, { useState } from "react";
-import Counter from './Counter';
 import Total from './Total';
 import "./App.css";
 
-  const App = () => {
-    let data=[
-      { id: 1, value: 1 },
-      { id: 2, value: 2},
-      { id: 3, value: 3 },
-      { id: 4, value: 4}
-    ]
+const App = () => {
 
-    const Counter = (props) => {
+  const [data, setData] = useState([
+    { id: 1, value: 1 },
+    { id: 2, value: 2 },
+    { id: 3, value: 3 },
+    { id: 4, value: 4 }
+  ])
 
-      const [value, setValue] = useState(props.value)
-      
-      const onIncrementHandler = () =>{
-          setValue(value + 1)
-          data = {id:1, value: data[0].value}
-              console.log(['updatedData'],data)
-            }
-      const onDecrementHandler = () =>{
-          setValue(value - 1)
-          }
-      
-          return (
-            <div className="counter">
-              <div className="counter-controls">
-                <button className="button is-danger is-small" onClick={onDecrementHandler}>-</button>
-                <button className="button is-success is-small" onClick={onIncrementHandler}>+</button>
-                {value}
-                {console.log(['Value'],data)}
-            </div>
-            </div>
-          );}
-       
- return (
-   <div>
-     {data.map(counter => ( 
-          <Counter 
-            key={counter.id} 
-            value={counter.value} 
-            />
-        ))}
-       <Total value={data[0].value + data[1].value + data[2].value + data[3].value} />
-   </div>
- )
+  const updateObjectAtIndex = (index, id, value) => {
+    // Finds the object inside the array and updates it
+    data[index] = {id, value}
+    
+    // Updates the data state
+    setData([...data])
   }
-  
+
+  return (
+    <div>
+      {data.map((counter, i) => (
+        <Counter
+          key={i}
+          index={i}
+          setData={updateObjectAtIndex}
+          obj={counter}
+        />
+      ))}
+      <Total value={data[0].value + data[1].value + data[2].value + data[3].value} />
+    </div>
+  )
+}
+
+const Counter = ({index, obj, setData}) => {
+  const onIncrementHandler = _ => {
+    setData(index, obj.id, obj.value + 1)
+  }
+
+  const onDecrementHandler = _ => {
+    setData(index, obj.id, obj.value - 1)
+  }
+
+  return (
+    <div className="counter">
+      <div className="counter-controls">
+        <button className="button is-danger is-small" onClick={onDecrementHandler}>-</button>
+        <button className="button is-success is-small" onClick={onIncrementHandler}>+</button>
+        {obj.value}
+      </div>
+    </div>
+  );
+}
+
 export default App;
 
